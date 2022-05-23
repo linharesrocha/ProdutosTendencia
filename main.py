95f8a3b519a44bce41a837757f194d8f8c7a6790
linharesrocha <guilhermelinharesr@gmail.com>
2022-05-22 22:11:23 -0300

    change heroku to googlechrome

--- main.py
+++ main.py
@@ -190,11 +190,15 @@ def start_streamlit():
 
 if __name__ == "__main__":
     # Settings Driver
-    option = Options()
-    option.headless = True
-    navegador = webdriver.Firefox(options=option)
     pd.set_option('mode.chained_assignment', None)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    navegador = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     navegador.maximize_window()
+
 
     # Settings Streamlit
     st.set_page_config(page_title='Tendencias', layout='wide')
