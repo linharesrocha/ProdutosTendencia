060f08f70aed9c84303fe1ce7e6ec8ea4416ea8e
linharesrocha <guilhermelinharesr@gmail.com>
2022-05-23 22:35:37 -0300

    add slack

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#from SQL.sql import start_sqlite
 import pandas as pd
 from datetime import datetime
 import re
